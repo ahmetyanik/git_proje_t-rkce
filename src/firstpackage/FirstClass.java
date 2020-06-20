d8435d82450d8564de08f9de24a7effe5ad4c4a8
ahmetyanik <67186720+ahmetyanik@users.noreply.github.com>
2020-06-20 21:16:33 +0200

    Update FirstClass.java
    ilk commit from remote repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -18,6 +18,8 @@ hocam kisaca ozetleyelim:
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
 		System.out.println("third commit from local repository");
+		System.out.println("first commit from remote repository");
+		
 		
 
 	}
